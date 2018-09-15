32595b2ca33458170337a025c7828e3fcf4c2121
Ian Dykes <ian.dykes@gmail.com>
2018-09-15 09:30:57 +0100

    Add PipelineBuilder (#1)
    Pipeline builder is inspired by ASP.Net's ApplicationBuilder but for a message context rather than a HttpContext

---
+++ src/Diadophis/MessageContext.cs
@@ -0,0 +1,9 @@
+﻿// Copyright 2018 Concrest
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Diadophis
+{
+    public abstract class MessageContext
+    {
+    }
+}
---
+++ src/Diadophis/PipelineBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2018 Concrest
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Diadophis
+{
+    public static class PipelineBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a terminal middleware delegate to the application's message pipeline.
+        /// </summary>
+        public static IPipelineBuilder Run(this IPipelineBuilder builder, MessageDelegate handler)
+        {
+            builder.Use(_ => handler);
+            return builder;
+        }
+    }
+}
