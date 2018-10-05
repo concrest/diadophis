f87c0d2c056c4313af50e415c8a03dac8c2261d9
Ian Dykes <ian.dykes@gmail.com>
2018-10-05 22:42:06 +0100

    Hacky avro producer

---
+++ examples/kafka/avro/AvroMessageProducer/Program.cs
@@ -0,0 +1,65 @@
+﻿// Copyright 2018 Concrest
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using SchemaExamples.Model;
+
+namespace AvroMessageProducer
+{
+    class Program
+    {
+        static long UnixNow() => (long)(DateTime.UtcNow - DateTime.UnixEpoch).TotalMilliseconds;
+
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "broker1:9092",
+            };
+
+            var avroConfig = new AvroSerdeProviderConfig
+            {
+                // Note: you can specify more than one schema registry url using the
+                // schema.registry.url property for redundancy (comma separated list). 
+                // The property name is not plural to follow the convention set by
+                // the Java implementation.
+                SchemaRegistryUrl = "http://schemaregistry1:8081",
+                // optional schema registry client properties:
+                SchemaRegistryRequestTimeoutMs = 5000,
+                SchemaRegistryMaxCachedSchemas = 10,
+                // optional avro serializer properties:
+                AvroSerializerBufferBytes = 50,
+                AvroSerializerAutoRegisterSchemas = true
+            };
+
+            using (var serdeProvider = new AvroSerdeProvider(avroConfig))
+            using (var producer = new Producer<string, HttpRequest>(
+                config,
+                serdeProvider.GetSerializerGenerator<string>(),
+                serdeProvider.GetSerializerGenerator<HttpRequest>()))
+            {
+                var httpRequest = new HttpRequest
+                {
+                    request_verb = HttpVerbs.Get,
+                    path = "/index.html",
+                    timestamp = UnixNow(),
+                    duration = 12.3,
+                    user_agent = "Foo"
+                };
+
+                // Alternatively - await ProduceAsync
+                producer.BeginProduce(
+                    "http-requests",
+                    new Message<string, HttpRequest>
+                    {
+                        Key = "user1@somewhere.io",
+                        Value = httpRequest
+                    });
+
+                producer.Flush(TimeSpan.FromSeconds(30));
+            }
+        }
+    }
+}
---
+++ examples/kafka/avro/SingleSchemaConsumer/Program.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2018 Concrest
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
+
+namespace SingleSchemaConsumer
+{
+    public class Program
+    {
+        private static readonly string HostingEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        private static readonly IConfiguration Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{HostingEnv}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static int Main(string[] args)
+        {
+            // Use appsettings and environment variables for logging config
+            var logBuilder = new LoggerConfiguration()
+               .ReadFrom.Configuration(Configuration)
+               .Enrich.FromLogContext();
+
+            // JSON formatting in Prod, Text in Dev:
+            if (HostingEnv == "Production")
+            {
+                logBuilder.WriteTo.Console(new JsonFormatter());
+            }
+            else
+            {
+                logBuilder.WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+                );
+            }
+
+            // Create logger here so fatal logs can be captured
+            Log.Logger = logBuilder.CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseSerilog();
+    }
+}
