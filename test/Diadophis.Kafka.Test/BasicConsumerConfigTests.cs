11dfab4263f16006ab5fcfde2144390b6f4c8496
Ian Dykes <ian.dykes@gmail.com>
2018-10-06 23:16:34 +0100

    Basic consumer config tests

---
+++ test/Diadophis.Kafka.Test/BasicConsumerConfigTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Diadophis.Kafka.Test
+{
+    public class BasicConsumerConfigTests
+    {
+        [Fact]
+        public void Creates_a_basic_consumer_strategy()
+        {
+            // Create a fake one for testing
+            var sut = new FakeBasicConsumerConfig();
+
+            var actual = sut.CreateConsumerStrategy();
+
+            Assert.IsType<BasicConsumerStrategy<string, string>>(actual);
+        }
+    }
+}
---
+++ test/Diadophis.Kafka.Test/FakeBasicConsumerConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Diadophis.Kafka.Test
+{
+    internal class FakeBasicConsumerConfig : BasicConsumerConfig<string, string>
+    {
+        public FakeBasicConsumerConfig()
+        {
+            BrokerUrls = "broker1";
+            ConsumerGroupId = "consumer";
+            Topics = new[] { "topic" };
+        }
+
+        public override void ConfigurePipeline(IPipelineBuilder pipe)
+        {
+            // configure fake pipeline...
+        }
+    }
+}
